e16788881bd63f5203de4056b646dddbdf0ef8f6
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-02-19 01:38:37 +0900

    ng generate @angular/material:table user-list-page

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserListPageComponent } from './containers/user-list-page/user-list-page.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserListPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class UserModule { }
