8fc25389167ea5b1c62e42ad359546a9b993206a
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-02-19 01:28:12 +0900

    ng generate @angular/material:nav nav

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,14 +3,28 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from '@my-app/core/containers/app/app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ]
 })
 export class CoreModule { }
