4d0653ef2c213d31058a13590816df86b0a7f37d
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-02-19 14:56:36 +0900

    update

--- src/app/users/reducers/users-api.reducer.ts
+++ src/app/users/reducers/users-api.reducer.ts
@@ -1,19 +1,38 @@
 import { Action, createReducer, on } from '@ngrx/store';
-
+import {
+  loadUsersApis,
+  loadUsersApisSuccess,
+  loadUsersApisFailure,
+} from '@my-app/users/actions/users-api.actions';
 
 export const usersApiFeatureKey = 'usersApi';
 
 export interface State {
-
+  loaded: boolean;
+  loading: boolean;
+  ids: string[];
 }
 
 export const initialState: State = {
-
+  loaded: false,
+  loading: false,
+  ids: [],
 };
 
 const usersApiReducer = createReducer(
   initialState,
-
+  on(loadUsersApis, state => ({
+    ...state,
+    loading: true,
+  })),
+  on(loadUsersApisSuccess, (state, { users }) => ({
+    loaded: true,
+    loading: false,
+    ids: users.map(user => user.id),
+  })),
+  on(loadUsersApisFailure, state => ({
+    ...state,
+  })),
 );
 
 export function reducer(state: State | undefined, action: Action) {
