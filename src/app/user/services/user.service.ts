117d568a4a058ed7809b4da7152e4f7c4fe190ae
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-02-19 12:33:00 +0900

    update user.service.ts

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { User } from '@my-app/user/reducers/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private apiPath = 'users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserList(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiPath);
+  }
 }
