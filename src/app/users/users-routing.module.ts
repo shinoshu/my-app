546b2d7f919e0bb7f3820329c3a3641d2c8f8ff4
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-02-19 19:52:21 +0900

    update users-routing.module.ts

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { UserListPageComponent } from '@my-app/users/containers/user-list-page/user-list-page.component'
+import { UserListPageComponent } from '@my-app/users/containers/user-list-page/user-list-page.component';
+import { UserFormPageComponent } from '@my-app/users/containers/user-form-page/user-form-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: UserListPageComponent,
     data: { title: 'User List' },
+  },
+  {
+    path: 'add',
+    component: UserFormPageComponent,
+    data: { title: 'User Form' },
   },
 ];
 
