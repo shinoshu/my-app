eeee349f45b6decef6ea1757263cfb2ad4d6b858
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-02-19 14:34:18 +0900

    update

--- src/app/users/actions/users-api.actions.ts
+++ src/app/users/actions/users-api.actions.ts
@@ -1,12 +1,14 @@
 import { createAction, props } from '@ngrx/store';
 
+import { User } from '@my-app/users/models/user.model';
+
 export const loadUsersApis = createAction(
   '[UsersApi] Load UsersApis'
 );
 
 export const loadUsersApisSuccess = createAction(
   '[UsersApi] Load UsersApis Success',
-  props<{ data: any }>()
+  props<{ users: User[] }>()
 );
 
 export const loadUsersApisFailure = createAction(
