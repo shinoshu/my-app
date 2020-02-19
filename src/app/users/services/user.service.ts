9a231331e2f89d390a9a93db06444d90153c36b2
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-02-19 20:39:08 +0900

    update user.service.ts

--- src/app/users/services/user.service.ts
+++ src/app/users/services/user.service.ts
@@ -12,11 +12,23 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
+  getUser(id: string): Observable<User> {
+    return this.http.get<User>(`${this.apiPath}/${id}`);
+  }
+
   getUserList(): Observable<User[]> {
     return this.http.get<User[]>(this.apiPath);
   }
 
   addUser(user: User): Observable<void> {
     return this.http.post<void>(this.apiPath, user);
+  }
+
+  updateUser(user: User): Observable<void> {
+    return this.http.post<void>(`${this.apiPath}/${user.id}`, user);
+  }
+
+  deleteUser(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiPath}/${id}`);
   }
 }
