9002298b9cd13b5524f976a9c100fba0449a075c
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-02-19 22:56:41 +0900

    update user-form-page

--- src/app/users/containers/user-form-page/user-form-page.component.ts
+++ src/app/users/containers/user-form-page/user-form-page.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 
+import { User } from '@my-app/users/models/user.model';
 import { selectUser } from '@my-app/users/actions/user.actions';
 import { addUsersApis } from '@my-app/users/actions/users-api.actions';
 import * as fromUsers from '@my-app/users/reducers/index';
@@ -14,23 +15,26 @@ import * as fromUsers from '@my-app/users/reducers/index';
   templateUrl: './user-form-page.component.html',
   styleUrls: ['./user-form-page.component.scss']
 })
-export class UserFormPageComponent implements OnDestroy {
+export class UserFormPageComponent implements OnInit, OnDestroy {
   form = this.fb.group({
     firstName: [null, Validators.required],
     lastName: [null, Validators.required],
   });
 
-  subscription: Subscription;
+  subscription = new Subscription();
 
   constructor(
     private fb: FormBuilder,
     private store: Store<fromUsers.State>,
     private route: ActivatedRoute,
   ) {
-    this.subscription = this.route.params
+    const subscription = this.route.params
       .pipe(map(params => selectUser({ id: params.id })))
-      .subscribe(action => store.dispatch(action));
+      .subscribe(action => this.store.dispatch(action));
+    this.subscription.add(subscription);
   }
+
+  ngOnInit() { }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
