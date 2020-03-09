25a932f417fbf8109409ff10f60c6e5182fb9397
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-09 21:27:46 +0900

    update

--- src/app/auth/reducers/auth.reducer.ts
+++ src/app/auth/reducers/auth.reducer.ts
@@ -1,19 +1,24 @@
 import { Action, createReducer, on } from '@ngrx/store';
-
+import * as AuthActions from '@my-app/auth/actions/auth-api.actions';
 
 export const authFeatureKey = 'auth';
 
 export interface State {
-
+  isLoggedIn: boolean;
 }
 
 export const initialState: State = {
-
+  isLoggedIn: false
 };
 
 const authReducer = createReducer(
   initialState,
-
+  on(AuthActions.loginSuccess,
+    (state, { data }) => ({ ...state, isLoggedIn: true })
+  ),
+  on(AuthActions.logoutSuccess,
+    (state, { data }) => ({ ...state, isLoggedIn: false })
+  ),
 );
 
 export function reducer(state: State | undefined, action: Action) {
