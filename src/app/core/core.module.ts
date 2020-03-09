79d768cfbb37739873b6f2b797d9176529b411c2
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-09 21:00:13 +0900

    update

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '@my-app/material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
 import { AppComponent } from '@my-app/core/containers/app/app.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -15,6 +17,8 @@ import { NavComponent } from './components/nav/nav.component';
     CommonModule,
     RouterModule,
     MaterialModule,
+    FlexLayoutModule,
+    NgxAuthFirebaseUIModule,
   ]
 })
 export class CoreModule { }
