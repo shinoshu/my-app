499a6962769d6938fe67b5890f12a89e85ed76b0
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-09 21:46:18 +0900

    update

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
 import { AuthRoutingModule } from './auth-routing.module';
 import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
+import * as fromAuth from '@my-app/auth/reducers';
 import { LoginPageComponent } from './containers/login-page/login-page.component';
 import { SignupPageComponent } from './containers/signup-page/signup-page.component';
 
@@ -10,6 +12,7 @@ import { SignupPageComponent } from './containers/signup-page/signup-page.compon
   declarations: [LoginPageComponent, SignupPageComponent],
   imports: [
     CommonModule,
+    StoreModule.forFeature(fromAuth.authFeatureKey, fromAuth.reducers),
     AuthRoutingModule,
     NgxAuthFirebaseUIModule,
   ]
--- src/app/auth/containers/login-page/login-page.component.ts
+++ src/app/auth/containers/login-page/login-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+
+import { loginSuccess } from '@my-app/auth/actions/auth-api.actions';
+import * as fromAuth from '@my-app/auth/reducers/index';
 
 @Component({
   selector: 'app-login-page',
@@ -8,9 +12,16 @@ import { Router } from '@angular/router';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  constructor(
+    private store: Store<fromAuth.State>,
+    public router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  loginSuccess(data: any) {
+    this.store.dispatch(loginSuccess({ data }));
+    this.router.navigate(['/users']);
+  }
 }
