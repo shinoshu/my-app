ee7f2c7c3d68dc98eabef49581025cf1a3dea7a7
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-09 19:59:04 +0900

    update

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginPageComponent } from '@my-app/auth/containers/login-page/login-page.component';
+import { SignupPageComponent } from '@my-app/auth/containers/signup-page/signup-page.component';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginPageComponent,
     data: { title: 'Login' },
+  },
+  {
+    path: 'signup',
+    component: SignupPageComponent,
+    data: { title: 'Signup' },
   },
 ];
 
